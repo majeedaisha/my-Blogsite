32f79077f1496673a2606c9a6361a7da2ec5ad1b
majeedaisha <majeedaisha44@gmail.com>
2022-03-29 08:04:26 -0700

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import withLayout from './withLayout'
+
+function Home() {
+  return (
+    <div>
+    <h1>welcome to my Homepage</h1>
+    <div>
+    <p>Kwame Nkrumah PC (21 September
+         1909[1][a] – 27 April 1972) was a Ghanaian politician, political theorist, and revolutionary. He was the first Prime Minister and President of Ghana, having led the Gold Coast to independence from Britain in 1957.[2] An influential advocate of Pan-Africanism, Nkrumah was a founding member of the Organization of African Unity and winner
+          of the Lenin Peace Prize from the Soviet Union in 1962.[3]
+    After twelve years abroad pursuing higher education, 
+    developing his political philosophy, 
+    and organizing with other diasporic pan-Africanists,
+     Nkrumah returned to the Gold Coast to begin his political 
+     career as an advocate of national independence.[4] He formed the 
+     Convention People's Party, which achieved rapid success through its 
+     unprecedented appeal to the common voter.[5]
+      He became Prime Minister in 1952 and retained the position when
+       Ghana declared independence from Britain in 1957. In 1960,
+     Ghanaians approved a new constitution and elected Nkrumah President.[6]</p>
+    
+    <p>Kwame Nkrumah PC (21 September
+         1909[1][a] – 27 April 1972) was a Ghanaian politician, political theorist, and revolutionary. He was the first Prime Minister and President of Ghana, having led the Gold Coast to independence from Britain in 1957.[2] An influential advocate of Pan-Africanism, Nkrumah was a founding member of the Organization of African Unity and winner
+          of the Lenin Peace Prize from the Soviet Union in 1962.[3]
+    After twelve years abroad pursuing higher education, 
+    developing his political philosophy, 
+    and organizing with other diasporic pan-Africanists,
+     Nkrumah returned to the Gold Coast to begin his political 
+     career as an advocate of national independence.[4] He formed the 
+     Convention People's Party, which achieved rapid success through its 
+     unprecedented appeal to the common voter.[5]
+      He became Prime Minister in 1952 and retained the position when
+       Ghana declared independence from Britain in 1957. In 1960,
+     Ghanaians approved a new constitution and elected Nkrumah President.[6]</p>
+    
+    <p>Kwame Nkrumah PC (21 September
+         1909[1][a] – 27 April 1972) was a Ghanaian politician, political theorist, and revolutionary. He was the first Prime Minister and President of Ghana, having led the Gold Coast to independence from Britain in 1957.[2] An influential advocate of Pan-Africanism, Nkrumah was a founding member of the Organization of African Unity and winner
+          of the Lenin Peace Prize from the Soviet Union in 1962.[3]
+    After twelve years abroad pursuing higher education, 
+    developing his political philosophy, 
+    and organizing with other diasporic pan-Africanists,
+     Nkrumah returned to the Gold Coast to begin his political 
+     career as an advocate of national independence.[4] He formed the 
+     Convention People's Party, which achieved rapid success through its 
+     unprecedented appeal to the common voter.[5]
+      He became Prime Minister in 1952 and retained the position when
+       Ghana declared independence from Britain in 1957. In 1960,
+     Ghanaians approved a new constitution and elected Nkrumah President.[6]</p>
+    
+    <p>Kwame Nkrumah PC (21 September
+         1909[1][a] – 27 April 1972) was a Ghanaian politician, political theorist, and revolutionary. He was the first Prime Minister and President of Ghana, having led the Gold Coast to independence from Britain in 1957.[2] An influential advocate of Pan-Africanism, Nkrumah was a founding member of the Organization of African Unity and winner
+          of the Lenin Peace Prize from the Soviet Union in 1962.[3]
+    After twelve years abroad pursuing higher education, 
+    developing his political philosophy, 
+    and organizing with other diasporic pan-Africanists,
+     Nkrumah returned to the Gold Coast to begin his political 
+     career as an advocate of national independence.[4] He formed the 
+     Convention People's Party, which achieved rapid success through its 
+     unprecedented appeal to the common voter.[5]
+      He became Prime Minister in 1952 and retained the position when
+       Ghana declared independence from Britain in 1957. In 1960,
+     Ghanaians approved a new constitution and elected Nkrumah President.[6]</p>
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>
+    
+    
+    </div>
+  )
+}
+
+export default withLayout(Home)
